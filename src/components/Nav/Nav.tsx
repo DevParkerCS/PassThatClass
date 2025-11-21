7a9612d16ccfb4639f810fc6a3278babde2a8f31
Parker Phillips <parkerphillips2003@gmail.com>
2025-11-20 21:06:14 -0800

    Updated nav styles

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,16 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Nav.module.scss";
 
 export const Nav = () => {
+  const nav = useNavigate();
   return (
     <nav className={styles.navWrapper}>
       <div>
         <h1 className={styles.navTitle}>PassThatClass</h1>
       </div>
 
-      <div>
-        <a className={styles.navItem}>Home</a>
-        <a className={styles.navItem}>About</a>
-        <a className={styles.navItem}>Dashboard</a>
+      <div className={styles.navItems}>
+        <p className={styles.navItem}>Home</p>
+        <p className={styles.navItem}>About</p>
+        <p className={styles.navItem} onClick={() => nav("/dashboard")}>
+          Dashboard
+        </p>
       </div>
     </nav>
   );

42f8da6a3c0f79510e583045274aabfb78c89a1b
Parker Phillips <parkerphillips2003@gmail.com>
2025-11-20 21:06:02 -0800

    Added base dashboard layout

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -5,8 +5,21 @@ import {
 import styles from "./Dashboard.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Nav } from "../../components/Nav/Nav";
+import { SearchBar } from "../../components/SearchBar/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
+  const classes = [
+    "Biology 201",
+    "CSCI 447",
+    "CSCI 497",
+    "ECON 206",
+    "Biology 201",
+    "CSCI 447",
+    "CSCI 497",
+    "ECON 206",
+  ];
+
   return (
     <div>
       <Nav />
@@ -15,20 +28,34 @@ export const Dashboard = () => {
         <h2 className={styles.dashboardTitle}>Dashboard</h2>
       </div>
 
-      <div className={styles.searchAlign}>
-        <div className={styles.searchWrapper}>
-          <FontAwesomeIcon
-            className={styles.searchIcon}
-            icon={faMagnifyingGlass}
-          />
-          <input
-            placeholder="Search for classes"
-            className={styles.searchInput}
-          />
-        </div>
+      <div className={styles.searchWrapper}>
+        <SearchBar p={"Search For Classes"} />
       </div>
 
-      <FontAwesomeIcon icon={faFolderOpen} />
+      <div className={styles.classesWrapper}>
+        <div className={styles.classesSizeWrapper}>
+          {classes.map((n, i) => {
+            return <Class name={n} key={i} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+type ClassProps = {
+  name: string;
+};
+
+export const Class = ({ name }: ClassProps) => {
+  const nav = useNavigate();
+
+  return (
+    <div className={styles.classWrapper} onClick={() => nav("/class")}>
+      <div className={styles.classTxtWrapper}>
+        <FontAwesomeIcon className={styles.classIcon} icon={faFolderOpen} />
+        <p className={styles.className}>{name}</p>
+      </div>
     </div>
   );
 };

e0c500fab344378da323b8191b61ee9a0bb82e35
Parker Phillips <parkerphillips2003@gmail.com>
2025-11-24 17:35:56 -0800

    Added/updated global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,27 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import "./styles/colors.css";
 import "./styles/borders.css";
+import "./styles/widths.module.scss";
 import "./styles/variables.module.scss";
 import { QuizContent } from "./Pages/Quiz/QuizContent";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Dashboard } from "./Pages/Dashboard/Dashboard";
 import { ClassFolder } from "./Pages/ClassFolder/ClassFolder";
+import { DataProvider } from "./context/DataContext/DataContext";
+import { NewQuiz } from "./Pages/NewQuiz/NewQuiz";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Router>
-    <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/quiz" element={<QuizContent />} />
-      <Route path="/class" element={<ClassFolder />} />
-    </Routes>
+    <DataProvider>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/class/:id" element={<ClassFolder />} />
+        <Route path="/class/:classId/quiz/:quizId" element={<QuizContent />} />
+        <Route path="/class/:id/newQuiz" element={<NewQuiz />} />
+      </Routes>
+    </DataProvider>
   </Router>
 );

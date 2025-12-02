1f4106ce68ef3274b4ae9e2f434ed733f7a7cec5
Parker Phillips <parkerphillips2003@gmail.com>
2025-12-02 09:38:36 -0800

    Updated pricing wording

--- src/Pages/LandingPage/components/Pricing/Pricing.tsx
+++ src/Pages/LandingPage/components/Pricing/Pricing.tsx
@@ -13,50 +13,52 @@ export const Pricing = () => {
 
         <div className={styles.sectionCardsWrapper}>
           <div className={styles.sectionCard}>
-            <p className={shared.cardName}>Basic Plan</p>
+            <p className={shared.cardName}>Free Starter</p>
             <p className={shared.cardValue}>Free</p>
 
             <ul className={shared.cardList}>
               <li className={shared.listItem}>
-                Generate <span className={shared.itemBold}>5 quizzes</span> from
-                notes
+                Generate{" "}
+                <span className={shared.itemBold}>7 AI-powered quizzes</span>{" "}
+                from your notes.
               </li>
               <li className={shared.listItem}>
-                Up to <span className={shared.itemBold}>5000 characters</span>{" "}
-                of notes per quiz.
+                Use up to{" "}
+                <span className={shared.itemBold}>5,000 characters</span> of
+                text per quiz.
               </li>
               <li className={shared.listItem}>
-                Input <span className={shared.itemBold}>1 image</span> of notes
+                Upload <span className={shared.itemBold}>1 image</span> of notes
                 per quiz.
               </li>
               <li className={shared.listItem}>
-                <span className={shared.itemBold}>Track your stats</span> for
-                your quizzes
+                <span className={shared.itemBold}>Track your quiz stats</span>{" "}
+                as you study.
               </li>
             </ul>
           </div>
 
           <div className={`${styles.sectionCard} ${styles.proSection}`}>
-            <p className={shared.cardName}>Pro Plan</p>
-            <p className={shared.cardValue}>$5/Month</p>
+            <p className={shared.cardName}>Pro Student</p>
+            <p className={shared.cardValue}>$5.99/Month</p>
 
             <ul className={shared.cardList}>
               <li className={shared.listItem}>
-                <span className={shared.itemBold}>Everything</span> from the
-                free plan and:
+                <span className={shared.itemBold}>Everything in Basic</span>,
+                plus:
               </li>
               <li className={shared.listItem}>
                 Generate up to{" "}
-                <span className={shared.itemBold}>100 quizzes</span> every month
+                <span className={shared.itemBold}>100 quizzes per month</span>.
               </li>
               <li className={shared.listItem}>
-                Input up to{" "}
+                Use up to{" "}
                 <span className={shared.itemBold}>20,000 characters</span> of
                 notes per quiz.
               </li>
               <li className={shared.listItem}>
-                Input up to <span className={shared.itemBold}>5 images</span> of
-                notes per quiz.
+                Upload up to <span className={shared.itemBold}>5 images</span>{" "}
+                of notes per quiz.
               </li>
             </ul>
           </div>
